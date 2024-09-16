1f85955cff6a0bf2fa4610cc928e8f74497dadbc
andy <andyestevez27@gmail.com>
2024-09-16 11:24:12 -0400

    Initial django-ninja api calls

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,9 +1,17 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
-@api.get("/hello")
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str = None
 
+@api.get("/hello")
 def hello(request):
     print(request)
     return "Hello World"
+
+@api.get("/me", response=UserSchema)
+def me(request):
+    return request.user
