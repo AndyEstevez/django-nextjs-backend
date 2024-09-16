8bcbf1cfe15275e5c74b91bd29faf108a1cf1b50
andy <andyestevez27@gmail.com>
2024-09-16 14:49:57 -0400

    Added access to api request from frontend

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # third party
     'ninja_extra',
     'ninja_jwt',
+    'corsheaders',
     # internal
     'waitlists',
 ]
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +58,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000", #nextjs
+    "http://127.0.0.1:3000",
+]
+
 
 TEMPLATES = [
     {
