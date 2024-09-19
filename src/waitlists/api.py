a9eed9ba2becbce9311557c8682beabdaac492f8
andy <andyestevez27@gmail.com>
2024-09-19 14:25:44 -0400

    Added create object from POST

--- src/waitlists/api.py
+++ src/waitlists/api.py
@@ -2,17 +2,28 @@ from typing import List
 from django.shortcuts import get_object_or_404
 from ninja import Router
 
+from ninja_jwt.authentication import JWTAuth
+
 from .models import WaitlistEntry
-from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema
+from .schemas import WaitlistEntryListSchema, WaitlistEntryDetailSchema, WaitlistEntryCreateSchema
 
 router = Router()
 
-@router.get("", response=List[WaitlistEntryListSchema])
+# /api/waitlists/
+@router.get("", response=List[WaitlistEntryListSchema], auth=JWTAuth())
 def list_waitlist_entries(request):
     qs = WaitlistEntry.objects.all()
     return qs
 
-@router.get("{entry_id}/", response=WaitlistEntryDetailSchema)
+# /api/waitlists/'
+@router.post("", response=WaitlistEntryDetailSchema)
+def create_waitlist_entry(request,
+data:WaitlistEntryCreateSchema):
+    print(data)
+    obj = WaitlistEntry.objects.create(**data.dict())
+    return obj
+
+@router.get("{entry_id}/", response=WaitlistEntryDetailSchema, auth=JWTAuth())
 def get_waitlist_entry(request, entry_id:int):
     obj = get_object_or_404(WaitlistEntry, id=entry_id)
     return obj
